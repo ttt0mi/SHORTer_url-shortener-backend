2bfe153667dc6f494e9e0d7a55c654c1b82d131d
tomi omolokun <tonmide96@gmail.com>
2026-02-17 18:09:25 +0100

    added extra variables to .env.example for development use. added ability to switch between env files based on current environment (development or production)

--- src/configuration/settings.py
+++ src/configuration/settings.py
@@ -1,10 +1,13 @@
+import os
 from functools import lru_cache
 
 from dotenv import find_dotenv
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-env_path = find_dotenv()
+ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
+env_file_map = {"development": ".env.dev", "production": ".env.prod",}
+env_path = find_dotenv(filename=env_file_map.get(ENVIRONMENT, ".env.dev"))
 
 class Settings(BaseSettings):
 	model_config = SettingsConfigDict(
@@ -14,8 +17,11 @@ class Settings(BaseSettings):
 	
 	database_uri: str = Field(..., alias="DATABASE_URI")
 	token_key: str = Field(..., alias="TOKEN_KEY")
-	allowed_hosts: list[str] = Field(default=["*"], alias="ALLOWED_HOSTS")
-	allowed_origins: list[str]= Field(default=["*"], alias="ALLOWED_ORIGINS")
+	
+	allowed_hosts: list[str] = Field(default_factory=lambda: ["*"], alias="ALLOWED_HOSTS")
+	allowed_origins: list[str] = Field(default_factory=lambda: ["*"], alias="ALLOWED_ORIGINS")
+	
+	environment: str = Field(default="development", alias="ENVIRONMENT")
 
 
 @lru_cache
